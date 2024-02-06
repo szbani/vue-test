b6bbd6e75b408878d34b73c3663cf0743aa60baa
szbani <szabodani2001@gmail.com>
2024-02-06 22:21:20 +0100

    proba

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "TaskItem"
+}
+</script>
+
+<template>
+  <div>
+    <h3>{{name}}</h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import "@material/web/all.js"
+
+createApp(App).mount('#app')
